5ede33901b762b78aae875f9d15d2040bd2652d5
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-11-28 11:55:09 +0100

    deletePost controller logic

--- controllers/feed.js
+++ controllers/feed.js
@@ -133,6 +133,34 @@ exports.updatePost = (req, res, next) => {
     });
 };
 
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+
+      // check logged in user
+      clearImage(post.imageUrl);
+      return Post.findByIdAndDelete(postId);
+    })
+    .then((result) => {
+      console.log(result);
+
+      res.status(200).json({ message: "Deleted Post." });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
 const clearImage = (filePath) => {
   filePath = path.join(__dirname, "..", filePath);
   fs.unlink(filePath, (err) => console.log(err));
