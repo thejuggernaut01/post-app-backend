f88889b495a6d106e49f77a9f140c453c0abff2f
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-11-29 09:34:02 +0100

    created signup login

---
+++ controllers/auth.js
@@ -0,0 +1,39 @@
+const User = require("../models/user");
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
+exports.signup = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+
+  const email = req.body.email;
+  const name = req.body.name;
+  const password = req.body.password;
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPw) => {
+      const user = new User({
+        email,
+        name,
+        password: hashedPw,
+      });
+
+      return user.save();
+    })
+    .then((result) => {
+      res.status(200).json({ message: "User created", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
